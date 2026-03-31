dd255ff93222d5534cc8a9cd7e769223c374328c
sonia singla <ssoniyaster@gmail.com>
2026-03-31 23:17:13 +0530

    Initial PWA commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,18 @@
-const CACHE_NAME = "chatbot-cache-v1";
-const urlsToCache = ["/","/index.html","/manifest.json","/icon-192.png","/icon-512.png"];
+const CACHE_NAME = 'mh-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", event => { event.waitUntil(self.clients.claim()); });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
